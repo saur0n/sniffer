64467809d29bfc0e335ae8be3c00b2df4b576d30
Sauron <git@saur0n.science>
2018-11-25 11:16:10 +0300

    Added StreamConnection.hpp

--- core/Sniffer.cpp
+++ core/Sniffer.cpp
@@ -21,6 +21,7 @@
 #include <utility>
 #include <vector>
 #include "Sniffer.hpp"
+#include "StreamConnection.hpp"
 
 using std::cerr;
 using std::cout;
@@ -385,79 +386,25 @@ std::mutex Connection::logMutex;
 
 /******************************************************************************/
 
-class StreamReader : public Reader, public Handler {
-public:
-    StreamReader(int fd, StreamReader &destination) : fd(fd), destination(destination) {}
-    ~StreamReader() {
-        close(fd);
-        fd=-1;
-        cv.notify_all();
-    }
-    int getDescriptor() const { return fd; }
-    void notify() {
-        if (fd>=0) {
-            char tempBuffer[4096];
-            auto retval=posix::read(fd, tempBuffer, sizeof(tempBuffer));
-            if (retval>0) {
-                std::unique_lock<std::mutex> lock(mutex);
-                buffer.append(tempBuffer, retval);
-            }
-            else {
-                close(fd);
-                fd=-1;
-            }
-            cv.notify_one();
-            if (retval>0)
-                posix::write(destination.getDescriptor(), tempBuffer, retval);
+void StreamReader::notify() {
+    if (fd>=0) {
+        char tempBuffer[4096];
+        auto retval=posix::read(fd, tempBuffer, sizeof(tempBuffer));
+        if (retval>0) {
+            std::unique_lock<std::mutex> lock(mutex);
+            buffer.append(tempBuffer, retval);
         }
+        else {
+            close(fd);
+            fd=-1;
+        }
+        cv.notify_one();
+        if (retval>0)
+            posix::write(destination.getDescriptor(), tempBuffer, retval);
     }
-    
-private:
-    StreamReader(const StreamReader &)=delete;
-    StreamReader &operator =(const StreamReader &)=delete;
-    void read(void * destination, size_t length) {
-        std::unique_lock<std::mutex> lock(mutex);
-        while ((fd>=0)&&(buffer.size()<length))
-            cv.wait(lock);
-        if (fd<0)
-            throw true;
-        memcpy(destination, buffer.data(), length);
-        buffer.erase(0, length);
-    }
-    
-    int fd;
-    StreamReader &destination;
-    string buffer;
-    std::mutex mutex;
-    std::condition_variable cv;
-};
+}
 
-/** Stream protocol sniffer **/
-class StreamConnection : public Connection {
-public:
-    /** Create TCP sniffer working as a forwarder **/
-    StreamConnection(Sniffer &controller, int client, HostAddress remote);
-    /** Create TCP sniffer working as SOCKS proxy **/
-    StreamConnection(Sniffer &controller, int client);
-    /** Close connections **/
-    ~StreamConnection();
-    
-private:
-    /** Client to server reader **/
-    StreamReader client;
-    /** Server socket descriptor **/
-    StreamReader server;
-    /** Connect to server **/
-    int initialize(HostAddress remote);
-    /** Accept SOCKS connection and connect to the target server **/
-    int acceptSocksConnection(int client);
-    /** Thread function **/
-    void threadFunc(ostream &log, bool incoming);
-    /** Polling thread **/
-    std::thread pollThread;
-    /** Polling thread worker **/
-    void pollThreadFunc();
-};
+/******************************************************************************/
 
 StreamConnection::StreamConnection(Sniffer &controller, int clientfd,
         HostAddress remote) : Connection(controller), client(clientfd, server),
---
+++ core/StreamConnection.hpp
@@ -0,0 +1,64 @@
+#ifndef __CORE_STREAMCONNECTION_HPP
+#define __CORE_STREAMCONNECTION_HPP
+
+#include "Sniffer.hpp"
+
+class StreamReader : public Reader, public Handler {
+public:
+    StreamReader(int fd, StreamReader &destination) : fd(fd), destination(destination) {}
+    ~StreamReader() {
+        close(fd);
+        fd=-1;
+        cv.notify_all();
+    }
+    int getDescriptor() const { return fd; }
+    void notify();
+    
+private:
+    StreamReader(const StreamReader &)=delete;
+    StreamReader &operator =(const StreamReader &)=delete;
+    void read(void * destination, size_t length) {
+        std::unique_lock<std::mutex> lock(mutex);
+        while ((fd>=0)&&(buffer.size()<length))
+            cv.wait(lock);
+        if (fd<0)
+            throw true;
+        memcpy(destination, buffer.data(), length);
+        buffer.erase(0, length);
+    }
+    
+    int fd;
+    StreamReader &destination;
+    std::string buffer;
+    std::mutex mutex;
+    std::condition_variable cv;
+};
+
+/** Stream protocol sniffer **/
+class StreamConnection : public Connection {
+public:
+    /** Create TCP sniffer working as a forwarder **/
+    StreamConnection(Sniffer &controller, int client, HostAddress remote);
+    /** Create TCP sniffer working as SOCKS proxy **/
+    StreamConnection(Sniffer &controller, int client);
+    /** Close connections **/
+    ~StreamConnection();
+    
+private:
+    /** Client to server reader **/
+    StreamReader client;
+    /** Server socket descriptor **/
+    StreamReader server;
+    /** Connect to server **/
+    int initialize(HostAddress remote);
+    /** Accept SOCKS connection and connect to the target server **/
+    int acceptSocksConnection(int client);
+    /** Thread function **/
+    void threadFunc(std::ostream &log, bool incoming);
+    /** Polling thread **/
+    std::thread pollThread;
+    /** Polling thread worker **/
+    void pollThreadFunc();
+};
+
+#endif
